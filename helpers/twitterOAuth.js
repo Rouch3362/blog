e04db62edd71ab50d14998ed343dbdd74621742b
unknown <rouchashoori@gmail.com>
2023-12-15 01:37:40 +0330

    twitter users now added to db

--- helpers/twitterOAuth.js
+++ helpers/twitterOAuth.js
@@ -1,11 +1,29 @@
 var passport = require('passport')
 var { Strategy } = require('passport-twitter');
+const { TwitterUser } = require('../db/schema');
+
 
 passport.use(new Strategy({
     consumerKey: process.env.TWITTER_API_KEY,
     consumerSecret: process.env.TWITTER_API_SECRET,
     callbackURL: 'http://localhost:3000/auth/twitter/callback'
   },
-  function(token, tokenSecret, profile, done) {
-    console.log(profile)
+  async function (token, tokenSecret, profile, done) {
+    let user = await TwitterUser.findOne({twitterId: profile.id})
+
+    if (!user){
+      user = await TwitterUser.create({username: profile.username , twitterId: profile.id , name: profile.displayName , profile_picture: profile.photos[0].value})
+    }
+    console.log(user)
+    done(null , user)
+
   }));
+
+
+passport.serializeUser((user, done) => {
+  done(null , {username: user.username , name: user.name , profilePicture: user.profile_picture})
+})
+
+passport.deserializeUser((user ,done) => {
+  done(null , user)
+})
