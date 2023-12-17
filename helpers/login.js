409398332837b44f7182743444b47952bacb4687
unknown <rouchashoori@gmail.com>
2023-12-17 18:42:31 +0330

    added message and error option to local strategy of passport

--- helpers/login.js
+++ helpers/login.js
@@ -5,17 +5,17 @@ const { ValidatePassword } = require("./authHelper")
 
 
 // username in this block of code can be email or actual username
-passport.use(new Strategy({} , async (username,  password , done) => {
+passport.use(new Strategy({passReqToCallback: true} , async (req, username,  password , done) => {
     console.log(username, password)
     // if user forgot to enter the credentials
     if (!username || !password){
-        return done(null , false , {error: "missing credentials"})
+        return done(null , false , req.flash("error", "missing credentials"))
     }
     // find user with email or username 
     const user = await UserSchema.findOne({$or: [{username}, {email: username}]})
     // if user not found we can show a message that user can not be found
     if (!user){
-        return done(null , false , {error: "user can not be found"})
+        return done(null , false , req.flash("error", "user can not be found"))
     }
 
 
@@ -23,7 +23,7 @@ passport.use(new Strategy({} , async (username,  password , done) => {
     const comparePassword = ValidatePassword(password , user.password)
 
     if (!comparePassword) {
-        return done(null , false , {error: "username or password is not correct"})
+        return done(null , false , req.flash("error", "username or password is not correct"))
     }
 
     done(null , user)
@@ -32,7 +32,7 @@ passport.use(new Strategy({} , async (username,  password , done) => {
 
 
 passport.serializeUser((user, done) => {
-    done(null , {username: user.username , name: user.name , profilePicture: user.profile_picture})
+    done(null , {id: user._id ,username: user.username , name: user.name , profilePicture: user.profile_picture})
 })
 
 passport.deserializeUser((user ,done) => {
