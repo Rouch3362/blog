50481de2f8b8362c23c5818471d5420358a9c708
unknown <rouchashoori@gmail.com>
2024-01-26 23:37:11 +0330

    upload image to uploads folder

---
+++ helpers/imageUploader.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const path = require('path')
+const multer  = require('multer')
+
+
+
+const storage = multer.diskStorage({
+    destination: (req , file , next) => {
+        fs.mkdirSync(`./uploads/${req.user.id}` , {recursive: true})
+        next(null , `uploads/${req.user.id}`)
+    },
+    filename: (req , file , next) =>{
+        next(null , file.fieldname + '-' + Date.now())
+    },
+})
+
+var upload = multer({storage: storage})
+
+
+
+module.exports = {
+    upload
+}
