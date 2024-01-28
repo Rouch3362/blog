fcb6a56597c96c6b043b0dbe4bd99343a70f42f1
unknown <rouchashoori@gmail.com>
2024-01-28 18:42:11 +0330

    implementing email sender with nodemailer

---
+++ helpers/emailSender.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer")
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "emailsender.for.auth.project@gmail.com",
+        pass: process.env.GMAIL_PASSWORD
+    }
+})
+
+
+const sendEmail = (email , subject , htmlMessage) => {
+    const info = {
+        from: "noreply@domail.com",
+        to: email,
+        subject: subject,
+        html: htmlMessage
+    }
+
+    transporter.sendMail(info , (err) => {
+        if (err) {
+            console.log(err)
+            return "an error occured" 
+        }
+        
+        return "email sent"
+    })
+}
+
+
+module.exports = {
+    sendEmail
+}
