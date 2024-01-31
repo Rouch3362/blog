6643480940d490fb7b529598015ae957622c849d
unknown <rouchashoori@gmail.com>
2024-02-01 00:05:41 +0330

    added reset password functionalities

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const {Router} = require("express")
 const router = Router()
-const { UserSchema } = require("../db/schema")
+const { UserSchema, TokenSchema } = require("../db/schema")
 const { HashPassword , ValidateEmail, CheckIfUserLoggedIn , PreventLoggingInAgain } = require("../helpers/authHelper")
 const passport = require("passport")
 const sanitizeHtml = require("sanitize-html")
+const { sendEmail } = require("../helpers/emailSender")
+const crypto = require("crypto")
 
 
 
@@ -20,6 +22,13 @@ router.route("/register")
     name = sanitizeHtml(name)
     username = sanitizeHtml(username)
     email = sanitizeHtml(email)
+
+    const usernameReg = /^[a-z0-9_\.]+$/
+
+    if (!username.match(usernameReg)) {
+        req.flash("error" , "invalid username")
+        return res.redirect("/auth/register")
+    }
     
     const userExist = await UserSchema.findOne({$or: [{email} , {username}]})
 
@@ -63,7 +72,7 @@ router.route("/login")
         message: req.flash('message') , 
         error: req.flash("error"),
         isAuthenticated: req.isAuthenticated()
-})
+    })
 })
 .post(passport.authenticate("local" , {failureFlash: true , failureRedirect: "/auth/login"}) , (req , res) => {
     req.flash("message" , `welcome back ${req.user.name}`)
@@ -86,6 +95,93 @@ router.post("/logout" , (req , res) => {
         req.flash("message" , "logged out successfully")
         res.redirect("/auth/login")
     })
+})
+
+
+router.route("/reset-password" , CheckIfUserLoggedIn)
+.get((req , res) => {
+    res.render("passwordReset.ejs" , {
+        error: req.flash("error"),
+        message: req.flash("message"),
+        isAuthenticated: req.isAuthenticated()
+    })
+})
+.post( async (req , res) => {
+    let { email } = req.body
+    const user = await UserSchema.findOne({email: email})
+
+    email = sanitizeHtml(email)
+
+    if (!email) {
+        req.flash("error" , "the email field is required")
+        return res.redirect(req.headers.referer || "/reset-password")
+    }
+
+    if (!ValidateEmail(email)) {
+        req.flash("error" , "email address is not valid please enter a valid one")
+        return res.redirect(req.headers.referer || "/reset-password")
+    }
+    
+    if (!user) {
+        req.flash("error" , "there is no user with this email address")
+        return res.redirect(req.headers.referer || "/reset-password")
+    }
+
+    let token = await TokenSchema.findOne({user: user.id})
+
+    if (!token) {
+        token = await TokenSchema.create({user: user.id , token: crypto.randomBytes(32).toString("hex")})
+    }
+
+    const link = `${process.env.BASE_URL}/auth/reset-password/${token.user}/${token.token}`
+
+    const emailMessage = sendEmail(email , "reset your password" , `<h1 style="text-align: center">Hello dear ${user.name}</h1> \ <p style="text-align:center;font-size:20px;"> this email message is request to reset the password of your account in blog website if you requested this action click on the link below <u>if you not just ignore it.</u></p> \ <div style="text-align:center;"><a href=${link} style="padding-left: 20px; padding-right:20px; padding-top: 10px; padding-bottom:10px; font-size: 20px;background-color: red; color: white; text-decoration: none; border-radius: 8px;">Reset My Password</a></div>`)
+    console.log(emailMessage)
+    if (!emailMessage) {
+        req.flash("error" , "an error occured please try again")
+        return res.redirect(req.headers.referer || "/reset-password")
+    }
+
+    req.flash("message" , "password reset link sent to your email")
+    res.redirect(req.headers.referer || "/reset-password")
+})
+
+router.route("/reset-password/:userId/:token" , CheckIfUserLoggedIn)
+.get(async(req , res) => {
+    const { userId , token } = req.params
+
+    const isValidToken = await TokenSchema.findOne({user:userId , token})
+
+    if (!isValidToken) {
+        req.flash("error" , "reset token is inavalid")
+        return res.redirect(req.headers.referer || "/reset-password")
+    }
+    res.render("newPassword.ejs" , {
+        error: req.flash("error") , 
+        message: req.flash("message") , 
+        isAuthenticated: req.isAuthenticated()
+    })
+})
+
+.post(async(req , res) => {
+    const { userId , token } = req.params
+
+    req.body.newPassword = sanitizeHtml(req.body.newPassword)
+
+    if (!req.body.newPassword) {
+        req.flash("error" , "new password is required")
+        return res.redirect(req.headers.referer || `/reset-password/${userId}/${token}`)
+    }
+
+
+    const password = HashPassword(req.body.newPassword)
+
+    const user = await UserSchema.findByIdAndUpdate(userId , {password: password})
+
+    await TokenSchema.deleteOne({user:userId, token})
+
+    req.flash("message" , "your password changed successfully. now login to your account")
+    res.redirect("/auth/login")
 })
 
 module.exports = router
