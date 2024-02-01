ac05cf9cf4ffd254adc8b6f74ffbdf96faee20ed
unknown <rouchashoori@gmail.com>
2024-02-01 21:43:59 +0330

    added fileFilter to multer for preventing uploading invalid files

--- helpers/imageUploader.js
+++ helpers/imageUploader.js
@@ -5,15 +5,24 @@ const multer  = require('multer')
 
 const storage = multer.diskStorage({
     destination: (req , file , next) => {
-        fs.mkdirSync(`./uploads/${req.user.id}` , {recursive: true})
-        next(null , `uploads/${req.user.id}`)
+        fs.mkdirSync(`./public/uploads/${req.user.id}` , {recursive: true})
+        next(null , `public/uploads/${req.user.id}`)
     },
     filename: (req , file , next) =>{
         next(null , file.fieldname + '-' + Date.now())
     },
 })
 
-var upload = multer({storage: storage})
+var upload = multer({
+    storage: storage,
+    fileFilter: (req , file , next) => {
+        if (!file.originalname.match(/\.(jpg|JPG|webp|jpeg|JPEG|png|PNG|gif|GIF|jfif|JFIF)$/)) {
+            req.fileValidationError = "only image files are allowed!"
+            return next(null , false)
+        }
+        next(null , true)
+    }
+})
 
 
 
